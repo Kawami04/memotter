37fefe876bb131b6c9f264ca74e210d4a14f9c65
kawamura <takumi.04kawamura@gmail.com>
2019-08-13 23:09:44 +0900

    password-form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   end
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
