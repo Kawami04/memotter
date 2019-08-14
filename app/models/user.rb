07643be5579a8913ddc272ee13282f2e5cde2e1e
kawamura <takumi.04kawamura@gmail.com>
2019-08-15 00:53:07 +0900

    login-session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true,  uniqueness: true, length: { maximum: 15 }
+  validates :email, presence: true, uniqueness: true, format: {with: /\w+@\w+\.\w+/}
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{4,32}\z/i}
 
   has_secure_password
+
 end
