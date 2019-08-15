6b6715dbc7bd7db63395013cfc22ccc8c30a0e7f
kawamura <takumi.04kawamura@gmail.com>
2019-08-15 11:25:25 +0900

    session-strong

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,35 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(email: session_params_email[:email])
+    if user && user.authenticate(session_params_password[:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
-
   end
+
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end
+
   private
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def session_params_email
+  params.require(:session).permit(:email)
+  end
+  def session_params_password
+  params.require(:session).permit(:password)
+  end
 end
