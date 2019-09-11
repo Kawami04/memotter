0dc9d5ec615f65fdefb3921e100528413830d3fc
kawamura <takumi.04kawamura@gmail.com>
2019-09-11 15:17:01 +0900

    show-destroy

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
   post '/favorites', to: 'favorites#create'
   delete '/favorites', to: 'favorites#destroy'
 
-  post "memos/:id/destroy", to: "memos#destroy" 
+  delete "memos/:id/destroy", to: "memos#destroy" 
 end
